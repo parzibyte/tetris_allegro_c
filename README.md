6fa13e0e5605af88f81b2521ba1b6c75d98c187a
Luis Cabrera Benito <parzibyte@gmail.com>
2021-02-13 23:02:35 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# tetris_allegro_c
+ Juego de Tetris en ANSI C (obviamente compatible con C++) usando Allegro (Trabajo en pogreso)
---
+++ main.c
@@ -0,0 +1,81 @@
+#include <allegro5/allegro5.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_primitives.h>
+#include <stdbool.h>
+
+#define ALTURA 500
+#define ANCHURA 500
+#define MEDIDA_CUADRO 50
+
+int main()
+{
+    al_init();
+    al_init_primitives_addon();
+    al_install_keyboard();
+
+    ALLEGRO_TIMER *timer = al_create_timer(1.0 / 30.0);
+    ALLEGRO_EVENT_QUEUE *queue = al_create_event_queue();
+
+    al_set_new_display_option(ALLEGRO_SAMPLE_BUFFERS, 1, ALLEGRO_SUGGEST);
+    al_set_new_display_option(ALLEGRO_SAMPLES, 8, ALLEGRO_SUGGEST);
+    al_set_new_bitmap_flags(ALLEGRO_MIN_LINEAR | ALLEGRO_MAG_LINEAR);
+    ALLEGRO_DISPLAY *disp = al_create_display(ANCHURA, ALTURA);
+    ALLEGRO_FONT *font = al_create_builtin_font();
+
+    al_register_event_source(queue, al_get_keyboard_event_source());
+    al_register_event_source(queue, al_get_display_event_source(disp));
+    al_register_event_source(queue, al_get_timer_event_source(timer));
+
+    bool redraw = true;
+    ALLEGRO_EVENT event;
+
+    al_start_timer(timer);
+    ALLEGRO_COLOR blanco = al_map_rgb_f(255, 255, 255);
+    ALLEGRO_COLOR rojo = al_map_rgb_f(255, 0, 0);
+
+    int cuadricula[5][5] = {
+        {1, 0, 1, 1, 1},
+        {1, 0, 1, 0, 1},
+        {1, 0, 0, 1, 1},
+        {1, 0, 1, 1, 1},
+        {1, 0, 1, 1, 0},
+    };
+
+    while (1)
+    {
+        al_wait_for_event(queue, &event);
+
+        if (event.type == ALLEGRO_EVENT_TIMER)
+            redraw = true;
+        else if ((event.type == ALLEGRO_EVENT_KEY_DOWN) || (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE))
+            break;
+
+        if (redraw && al_is_event_queue_empty(queue))
+        {
+            al_clear_to_color(al_map_rgb(0, 0, 0));
+            al_draw_text(font, al_map_rgb(255, 255, 255), 0, 0, 0, "Hola mundo");
+            int i, j;
+            float x = 0, y = 100;
+            for (i = 0; i < 5; i++)
+            {
+                for (j = 0; j < 5; j++)
+                {
+                    al_draw_filled_rectangle(x, y, x + MEDIDA_CUADRO, y + MEDIDA_CUADRO, cuadricula[i][j] == 0 ? blanco : rojo);
+                    x += MEDIDA_CUADRO;
+                }
+                x = 0;
+                y += MEDIDA_CUADRO;
+            }
+            al_flip_display();
+
+            redraw = false;
+        }
+    }
+
+    al_destroy_font(font);
+    al_destroy_display(disp);
+    al_destroy_timer(timer);
+    al_destroy_event_queue(queue);
+
+    return 0;
+}
